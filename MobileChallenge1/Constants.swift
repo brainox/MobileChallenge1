e08fd02b605da58a3302492a61257eb9bc46ba2f
brainox <macmartins081@gmail.com>
2021-06-12 13:43:18 +0100

    feat: added images to the cells

---
+++ MobileChallenge1/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  MobileChallenge1
+//
+//  Created by Decagon on 11/06/2021.
+//
+
+import Foundation
+
+
+enum constants {
+    static let url = "http://codingtest.fretello.com:3000/data/sessions.json"
+    static let fontRegular = "AvenirNext-Regular"
+    static let fontBold = "AvenirNext-Bold"
+    static let maxOutput =  "The maximum output increase is"
+    static let urlStr = "http://codingtest.fretello.com:3000/img/"
+    static let png = ".png"
+}
+
--- MobileChallenge1/Controller/RootViewController.swift
+++ MobileChallenge1/Controller/RootViewController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import Kingfisher
 
 class RootViewController: UIViewController{
     @IBOutlet weak var maxOutputLabel: UILabel!
-    var session = SessionResponse()
-    
-
     @IBOutlet weak var table: UITableView!
+    
+    var session = SessionResponse()
+    var sessionExercise: Exercise?
+    
+    var imageList: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +25,7 @@ class RootViewController: UIViewController{
     }
     
     func getData() {
-        let url = URL(string: "http://codingtest.fretello.com:3000/data/sessions.json")
+        let url = URL(string: constants.url)
         URLSession.shared.dataTask(with: url!) { (data, response, error) in
             guard let data = data, error == nil else {
                 print("Something went wrong")
@@ -35,15 +38,11 @@ class RootViewController: UIViewController{
             } catch {
                 print("Error: \(error)")
             }
-            
             guard let result = json else {
                 return
             }
             let reversed = result.reversed()
             self.session += reversed
-            
-            
-            
             var exercisesArr = [[Int]]()
             for practiceSession in result {
                 var exerciseBpmArr = [Int]()
@@ -53,7 +52,6 @@ class RootViewController: UIViewController{
                 exercisesArr.append(exerciseBpmArr)
             }
             let maxO = MaxPercentBPMDifference().maxAverage(exercisesArr: exercisesArr)
-            
             DispatchQueue.main.async {
                 self.maxOutputLabel.text = "The maximum output is: \(maxO)"
                 self.table.reloadData()
@@ -63,20 +61,49 @@ class RootViewController: UIViewController{
 }
 
 extension RootViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return session.count
+        return self.session[section].exercises.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "sessionCell") as! SessionCell
+        let currentCell = session[indexPath.section].exercises[indexPath.row]
         cell.setUp(with: session[indexPath.row])
-//        cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
+        cell.session = currentCell
+        cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 3
         cell.maxOutputView.layer.cornerRadius = cell.maxOutputView.frame.height / 2
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        self.session.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let sectionHeader = UIView()
+        sectionHeader.backgroundColor = #colorLiteral(red: 0.1490196078, green: 0.1333333333, blue: 0.1294117647, alpha: 1)
+        let sessionHeaderLabel = UILabel()
+        sessionHeaderLabel.text = (session[section].name).uppercased()
+        sessionHeaderLabel.textColor =  #colorLiteral(red: 0.9333333333, green: 0.768627451, blue: 0.462745098, alpha: 1)
+        sessionHeaderLabel.font = UIFont(name: constants.fontBold, size: 18)
+        sessionHeaderLabel.frame = CGRect(x: 20, y: -10 , width: self.view.frame.width, height: 50)
+        sectionHeader.addSubview(sessionHeaderLabel)
+        
+        let exerciseLabel = UILabel()
+        exerciseLabel.text = String((session[section].practicedOnDate).prefix(10) )
+        exerciseLabel.textColor =  #colorLiteral(red: 0.9333333333, green: 0.768627451, blue: 0.462745098, alpha: 1)
+        exerciseLabel.font = UIFont(name: constants.fontRegular, size: 13)
+        exerciseLabel.frame = CGRect(x: 20, y: 15 , width: self.view.frame.width, height: 50)
+        sectionHeader.addSubview(exerciseLabel)
+        return sectionHeader
     }
 }
